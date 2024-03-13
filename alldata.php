56d27d793168b4fc13872567327f230f92a25445
zhenllyalvarez <dev.zhenllyalvarez@gmail.com>
2024-03-13 15:13:25 +0800

    3/13/24

---
+++ alldata.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>SocialSnap</title>
+</head>
+
+<body>
+<div class="min-h-screen flex items-center justify-center flex-col">
+    <h1 class="font-semibold text-7xl text-center uppercase mb-8">all social</h1>
+    <button class="bg-indigo-400 text-white py-3 px-5 mb-8 rounded">add social</button>
+        <div class="w-3/4 sm:w-1/2 relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Social Media
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Password
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Facebook
+                        </th>
+                        <td class="px-6 py-4">
+                            test@gmail.com
+                        </td>
+                        <td class="px-6 py-4">
+                            test123
+                        </td>
+                        <td class="px-6 py-4">
+                            <button class="bg-blue-400 hover:bg-blue-500 text-white py-2 px-3 rounded">Update</button>
+                            <button class="bg-red-400 hover:bg-red-500 text-white py-2 px-4 rounded">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE socialsap
+
+CREATE TABLE socials(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    socialmedia VARCHAR(60) NOT NULL,
+    email VARCHAR(60),
+    password VARCHAR(50)
+);
